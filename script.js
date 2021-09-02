bb5390b76767dcd3627063642fc9d98333154c98
Sloth247 <86727485+Sloth247@users.noreply.github.com>
2021-09-02 18:24:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-const email = document.getElementById('email');
-const error = document.getElementById('error-message');
-const errorArrow = document.getElementById('error-arrow')
-const form = document.getElementById('form');
-const arrow = document.getElementById('arrow')
+const form = document.querySelector(".main__form");
+const input = document.querySelector(".main__form--input");
 
-//email.oninvalid = invalid;
+const errorMsg = document.querySelector(".main__form--error-message");
+const errorIcon = document.querySelector(".error-icon");
 
-//form.onsubmit = submit;
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    if(email.validity.typeMismatch || email.validity.valueMissing) {
-        // email.setCustomValidity('Please provide a valid email.');
-        error.classList.remove('hidden');
-        errorArrow.classList.remove('hidden');
-    } else  {
-        email.setCustomValidity(''); 
-    
-    } 
-    
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  emailValidation();
 });
 
+const isEmail = (input) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(input).toLowerCase());
+};
+
+const emailValidation = () => {
+  if (input.value.trim() === "") {
+    errorMsg.innerHTML = "Email cannot be empty";
+
+    input.classList.add(".warning-active");
+    errorIcon.classList.add("error-show");
+    return false;
+  } else if (!isEmail(input.value.trim())) {
+    errorMsg.innerHTML = "please provide a valid email address";
+
+    input.classList.add(".warning-active");
+    errorIcon.classList.add("error-show");
+    return false;
+  } else if (isEmail(input.value)) {
+    errorMsg.innerHTML = "Success! Valid email address";
+
+    input.classList.remove(".warning-active");
+    errorIcon.classList.remove("error-show");
+    return true;
+  }
+  return false;
+};
+
+input.addEventListener("input", () => {
+  errorMsg.innerHTML = "";
+  input.classList.remove(".warning-active");
+  errorIcon.classList.remove("error-show");
+});
